dbb98aa526404bed156b1a30f4df7d8d483c5dee
Aviram Eldar <107575552+avirameldar@users.noreply.github.com>
2026-01-24 20:57:20 +0200

    Fix deployment config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  // השורה הזו חשובה כדי לייצר תיקיית out סטטית
 };
 
 export default nextConfig;
